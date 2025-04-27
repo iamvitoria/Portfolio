16e51dcedcb3c9dfbf1321260d72fa6cf55df10a
Vitória Camara <120670231+iamvitoria@users.noreply.github.com>
2025-04-27 18:52:52 -0300

    feat add content on about.html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector(".nav-links");
+const carouselContents = document.querySelectorAll('.carousel-content');
+const btnRight = document.querySelector('.carousel-button.right');
+const btnLeft = document.querySelector('.carousel-button.left');
+let currentIndex = 0;
 
-    menuToggle.addEventListener("click", function() {
-        navLinks.classList.toggle("active");
-    });
+function showContent(index) {
+    carouselContents.forEach(content => content.classList.remove('active'));
+    carouselContents[index].classList.add('active');
+}
+
+// Botão para direita
+btnRight.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % carouselContents.length;
+    showContent(currentIndex);
+});
+
+// Botão para esquerda
+btnLeft.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + carouselContents.length) % carouselContents.length;
+    showContent(currentIndex);
 });
